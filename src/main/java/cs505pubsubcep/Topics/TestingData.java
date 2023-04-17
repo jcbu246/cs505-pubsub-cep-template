48a7db141348a5609efc3210866f151897c0ad55
jcbu246 <62860999+jcbu246@users.noreply.github.com>
2023-04-17 13:29:46 -0400

    Update TestingData.java

--- src/main/java/cs505pubsubcep/Topics/TestingData.java
+++ src/main/java/cs505pubsubcep/Topics/TestingData.java
@@ -2,7 +2,7 @@ package cs505pubsubcep.Topics;
 
 import java.util.List;
 
-public class TestingData {
+public class patient_list {
 
     public int testing_id;
     public String patient_name;
@@ -12,8 +12,27 @@ public class TestingData {
     public List<String> contact_list;
     public List<String> event_list;
 
-    public  TestingData() {
+    public  patient_list() {
 
     }
-
-}
+    
+public class hospital_list {
+    
+    public int hospital_id;
+    public String paitent_mrn;
+    public String patient_name;
+    public int patient_status;
+    
+    public hospital_list() {
+        
+    }
+    
+public class vax_list {
+    
+    public int vaccination_id;
+    public String patient_mrn;
+    public String patient_name;
+    
+    public vax_list() {
+        
+    }
